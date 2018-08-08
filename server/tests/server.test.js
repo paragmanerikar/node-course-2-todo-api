ded7560e0e2f325198ca3bce7735c8c771dad292
Parag Manerikar <Paragm@mkclindia.local>
2018-08-08 22:06:33 +0530

    Added GET /todos tests

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -5,8 +5,15 @@ const { app } = require('./../server');
 const { Todo } = require('./../models/todo');
 //const { User } = require('./../models/user');
 
+const todos = [
+    { text: 'first test todo' },
+    { text: 'second test todo' }
+];
+
 beforeEach((done) => {
-    Todo.remove({}).then(() => done());
+    Todo.remove({}).then(() => {
+        return Todo.insertMany(todos);
+    }).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -24,7 +31,7 @@ describe('POST /todos', () => {
                 if (err) {
                     return done(err);
                 }
-                Todo.find().then((todos) => {
+                Todo.find({ text }).then((todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -47,9 +54,23 @@ describe('POST /todos', () => {
                     return done(err);
                 }
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch((e) => done(e));
             });
+    });
+});
+
+
+
+describe('POST /todos', () => {
+    it('should gt all todos', (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(2)
+            })
+            .end(done);
     });
 })
