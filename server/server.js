0f60df9a27d4ba27e5de9d9502b106eaa7929a77
Parag Manerikar <Paragm@mkclindia.local>
2018-08-08 21:38:22 +0530

    Test POST /todos route

--- server/server.js
+++ server/server.js
@@ -17,11 +17,15 @@ app.post('/todos', (req, res) => {
         .then((doc) => {
             res.send(doc);
         }, (e) => {
-            res.status(400).send(e);
+            res.status(200).send(e);
         });
 })
 
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
-})
+});
+
+module.exports = {
+    app
+}
